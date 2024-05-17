5e7992f3500d687e3ba2fd1ec883a79789facde4
bello hadi <bellohadi82@gmail.com>
2024-05-17 18:02:06 +0100

    :bug: fixed the responsivesness issue with the get started page

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -119,7 +119,7 @@ const Header = ()=>{
                                 </div>
                             </div>
                         </div>
-                    ) 
+                    )
                 }
 
             </div>
--- src/Pages/GetStarted/GetStarted.jsx
+++ src/Pages/GetStarted/GetStarted.jsx
@@ -8,13 +8,13 @@ import { FaLocationDot } from "react-icons/fa6";
 const GetStarted = ()=>{
     return(
         <>
-            <div className="flex flex-col items-center my-20">
-                <div className="flex sm:flex-col justify-between">
+            <div className="flex flex-col items-center my-20 sm:mx-10">
+                <div className="flex sm:flex-col justify-between sm:w-full">
                     <div className="flex flex-col">
                         <h4 className="flex-wrap w-[517px] sm:w-fit text-5xl font-bold text-[#252524]">
                             We've got the perfect space for you
                         </h4>
-                        <p className="w-[413px] flex-wrap font-normal text-base text-[#252524] my-4">
+                        <p className="w-[413px] sm:w-fit  flex-wrap font-normal text-base text-[#252524] my-4">
                             At Enterprise Hubs, we're all about making things easy and flexible. 
                             Whether you're looking for a permanent office for your team or just a 
                             virtual office address, we've got a spot waiting for you in our welcoming 
@@ -24,15 +24,15 @@ const GetStarted = ()=>{
                             <img src={images.book} alt="book" className="mr-3" />Book your space
                         </Link>
                     </div>
-                    <img src={images.started1} alt="female picture" className="w-[458px] h-[375px]" />
+                    <img src={images.started1} alt="female picture" className="w-[458px] sm:w-fit h-[375px] sm:h-fit" />
                 </div>
-                <div className="flex sm:flex-col justify-between my-10">
-                    <img src={images.started2} alt="female picture" className="w-[458px] h-[375px]" />
-                    <div className="flex flex-col ml-20">
+                <div className="flex sm:flex-col justify-between my-10 sm:w-full">
+                    <img src={images.started2} alt="female picture" className="w-[458px] sm:w-fit h-[375px] sm:h-fit" />
+                    <div className="flex flex-col ml-20 sm:ml-0">
                         <h4 className="flex-wrap w-[517px] text-5xl font-bold text-[#252524]">
                             Virtual Office
                         </h4>
-                        <p className="w-[413px] flex-wrap font-normal text-base text-[#252524] my-4">
+                        <p className="w-[413px] sm:w-fit flex-wrap font-normal text-base text-[#252524] my-4">
                             Take control of your business, by having a business address in the heart 
                             of Lagos Nigeria.
                         </p>
@@ -41,12 +41,12 @@ const GetStarted = ()=>{
                          </Link>
                     </div>
                 </div>
-                <div className="flex sm:flex-col justify-between">
+                <div className="flex sm:flex-col justify-between sm:w-full">
                     <div className="flex flex-col jusitify-start">
-                        <h4 className="flex-wrap w-[517px] text-5xl font-bold text-[#252524]">
+                        <h4 className="flex-wrap w-[517px] sm:w-fit text-5xl font-bold text-[#252524]">
                             Where entrepreneurs go to grow
                         </h4>
-                        <p className="w-[413px] flex-wrap font-normal text-base text-[#252524] my-4">
+                        <p className="w-[413px] sm:w-fit flex-wrap font-normal text-base text-[#252524] my-4">
                             We've redefined the landscape for emerging enterprises in Nigeria and 
                             beyond, offering a unique blend of online and physical resource centers
                              designed to propel your business forward.
@@ -55,7 +55,7 @@ const GetStarted = ()=>{
                             Visit Website<span><GoArrowUpRight /></span>
                         </Link>
                     </div>
-                    <img src={images.started3} alt="female picture" className="w-[458px] h-[375px]" />
+                    <img src={images.started3} alt="female picture" className="w-[458px] sm:w-fit h-[375px] sm:h-fit" />
                 </div>
             </div>
         </>
