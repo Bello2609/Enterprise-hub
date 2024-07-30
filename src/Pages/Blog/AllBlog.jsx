879fba312d1898b63bd136340c52a879ba639ed3
bello hadi <bellohadi82@gmail.com>
2024-07-30 20:57:53 +0100

    :art: restored all the lost files

---
+++ src/Pages/Blog/AllBlog.jsx
@@ -0,0 +1,115 @@
+import React from "react";
+import * as images from "../../image";
+
+const AllBlog = ()=>{
+    return(
+        <>
+            <div className="flex flex-col items-center bg-[#F3F9F1] py-20">
+                <div className="flex justify-evenly px-20 sm:px-10 w-full">
+                    <div className="flex flex-col items-start w-full">
+                        <h4 className="flex-wrap w-[517px] sm text-left text-6xl font-bold text-[#252524]">
+                            News and Insights
+                        </h4>
+                        <p className="w-[413px] sm:w-fit flex-wrap font-normal text-base text-[#252524] my-5">
+                            We regularly publish business tips and corporate articles for your 
+                            consumption
+                        </p>
+                    </div>
+                  
+                </div>
+                <div className="grid grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-4 px-2">
+                        <div className="flex sm:hidden flex-col bg-[#fff] col-span-2 md:col-span-1 sm:col-span-1 bg-[url('/src/assets/roomInterior.png')] h-[446px] w-[757px] md:w-fit sm:w-fit  rounded-xl p-10 ">
+                            {/* <div className="bg-[#000000] w-full h-full opacity-40 rounded-xl"> */}
+                                <p className="font-sans text-base text-[#81C167] font-bold">
+                                    ARTICLE
+                                </p>
+                                <h4 className="font-sans font-bold text-xl text-[#ffffff] w-[312px] my-5">
+                                    Enterprise Hubs Hosts Baykins Pharmacy
+                                </h4>
+                                <p className="text-[#DEDEDE] font-sans font-normal w-[301px]">
+                                    We had the pleasure of serving Baykins Pharmacy with our best as they held 
+                                    their staff retreat on Friday September …
+                                </p>
+                            {/* </div> */}
+                        </div>
+                        {/* on mobile */}
+                        <div className="hidden sm:flex flex-col h-[446px] bg-[#fff] bg-[url('/src/assets/roomInterior.png')] rounded-xl p-5">
+                            {/* <div className="bg-[#000000] w-full h-full opacity-40 rounded-xl"> */}
+                                <p className="font-sans text-base text-[#81C167] font-bold">
+                                    ARTICLE | 👨🏼‍🦰Barki Taiwo | 12 Jan 2024
+                                </p>
+                                <h4 className="font-sans font-bold text-xl text-[#ffffff] w-[312px] my-5">
+                                    Enterprise Hubs Hosts Baykins Pharmacy
+                                </h4>
+                                <p className="text-[#DEDEDE] font-sans font-normal w-[301px]">
+                                    We had the pleasure of serving Baykins Pharmacy with our best as 
+                                    they held their staff retreat on Friday September …
+                                </p>
+                            {/* </div> */}
+                        </div>
+                        <div className="flex flex-col bg-[#fff] rounded-xl border border-[#DADADA] h-fit">
+                            <img src={images.manbag} alt="A man carrying a bag" className="rounded-t-xl" />
+                            <div className="p-8">
+                                <p className="font-sans text-base text-[#81C167] font-bold">
+                                    NEWS
+                                </p>
+                                <h4 className="font-sans font-bold text-xl text-[#252524] w-[312px] my-5">
+                                    Enterprise Hubs Hosts Baykins Pharmacy
+                                </h4>
+                                <p className="text-[#616161] font-sans font-normal w-[301px]">
+                                    We had the pleasure of serving Baykins Pharmacy with our best as 
+                                    they held their staff retreat on Friday September …
+                                </p>
+                            </div>
+                        </div>
+                        <div className="flex flex-col  bg-[#fff] rounded-xl border border-[#DADADA] h-fit">
+                            <img src={images.manbag} alt="A man carrying a bag" className="rounded-t-xl" />
+                            <div className="p-8">
+                                <p className="font-sans text-base text-[#81C167] font-bold">
+                                    NEWS
+                                </p>
+                                <h4 className="font-sans font-bold text-xl text-[#252524] w-[312px] my-5">
+                                    Enterprise Hubs Hosts Baykins Pharmacy
+                                </h4>
+                                <p className="text-[#616161] font-sans font-normal w-[301px]">
+                                    We had the pleasure of serving Baykins Pharmacy with our best as 
+                                    they held their staff retreat on Friday September …
+                                </p>
+                            </div>
+                        </div>
+                        <div className="flex flex-col bg-[#fff] rounded-xl border border-[#DADADA] h-fit">
+                            <img src={images.manbag} alt="A man carrying a bag" className="rounded-t-xl" />
+                            <div className="p-8">
+                                <p className="font-sans text-base text-[#81C167] font-bold">
+                                    NEWS
+                                </p>
+                                <h4 className="font-sans font-bold text-xl text-[#252524] w-[312px] my-5">
+                                    Enterprise Hubs Hosts Baykins Pharmacy
+                                </h4>
+                                <p className="text-[#616161] font-sans font-normal w-[301px]">
+                                    We had the pleasure of serving Baykins Pharmacy with our best as 
+                                    they held their staff retreat on Friday September …
+                                </p>
+                            </div>
+                        </div>
+                        <div className="flex flex-col bg-[#fff] rounded-xl border border-[#DADADA] h-fit">
+                            <img src={images.manbag} alt="A man carrying a bag" className="rounded-t-xl" />
+                            <div className="p-8">
+                                <p className="font-sans text-base text-[#81C167] font-bold">
+                                    NEWS
+                                </p>
+                                <h4 className="font-sans font-bold text-xl text-[#252524] w-[312px] my-5">
+                                    Enterprise Hubs Hosts Baykins Pharmacy
+                                </h4>
+                                <p className="text-[#616161] font-sans font-normal w-[301px]">
+                                    We had the pleasure of serving Baykins Pharmacy with our best as 
+                                    they held their staff retreat on Friday September …
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+            </div>
+        </>
+    );
+}
+export default AllBlog;
---
+++ src/Pages/UserProfileEdit/UserProfileEdit.jsx
@@ -0,0 +1,77 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { FaPhone } from "react-icons/fa6";
+import { IoMail } from "react-icons/io5";
+import { LuUpload } from "react-icons/lu";
+import { IoCheckmarkCircle } from "react-icons/io5";
+import { FaRegTimesCircle } from "react-icons/fa";
+import * as images from "../../image"
+import FormInput from "../../Components/FormInput/FormInput";
+import FormNumber from "../../Components/FormNumber/FormNumber";
+
+
+
+
+const UserProfileEdit= ()=>{
+    return(
+        <>
+             <div className="flex flex-col bg-[#DFDFDF] items-center justify-center w-full h-fit py-10">
+                <div className="flex flex-col bg-[#fff] rounded-b-2xl">
+                    <div className="flex items-center justify-between  w-[807px] h-[150px] bg-gradient-to-r from-[#FFFCB8] to-[#F5D0CA]">
+                        <div className="sm:hidden w-28 h-28"></div>
+                        <p className="sm:hidden font-sans text-base font-medium pl-16">Good afternoon, Ogechi</p>
+                        <img src={images.group6} className="sm:hidden h-full border-2 border-red-500" alt="" />
+                    </div>
+                    <div className="sm:flex sm:items-center rounded-full bg-[#ffffff] w-fit mt-[-80px] sm:ml-[320px] ml-10">
+                        <img src={images.profilePic} alt="user profile picture" />
+                    </div>
+                    <div className="flex flex-col sm:items-center p-10">
+                        <FormInput type="email" label="Email " />
+                        <FormInput type="password" label="Password " />
+                        <div className="flex sm:flex-col sm:items-center justify-evenly w-full">
+                            <FormInput type="email" label="Gender " />
+                            <FormInput type="password" label="Role " />
+                        </div>
+                        <div className="flex sm:flex-col sm:items-center justify-between w-full">
+                            <FormInput type="email" label="First Name " />
+                            <FormInput type="password" label="Last Name " />
+                        </div>
+                        <div className="flex sm:flex-col sm:items-center justify-between w-full">
+                             <FormNumber
+                                label="Phone Number"
+                            />
+                            <FormInput type="text" label="Company Name " />
+                        </div>
+                        <Link className="flex justify-center items-center my-5 w-full h-[57px] text-[#252524] font-medium bg-[#F4F4F4] p-3 w-40 h-12 rounded-md ">
+                            <span className="mr-2"><LuUpload /></span>CAC Registration.pdf<span className="ml-2"><FaRegTimesCircle /></span>
+                        </Link>
+                        <Link className="flex justify-center items-center w-full h-[57px] text-[#252524] font-medium bg-[#F4F4F4] p-3 w-40 h-12 rounded-md ">
+                            <span className="mr-2"><LuUpload /></span>Valid Card.pdf<span className="ml-2"><FaRegTimesCircle /></span>
+                        </Link>
+                    </div>
+                    
+                </div>
+                <div className="flex flex-col w-[807px] h-fit bg-[#fff] rounded-2xl mt-5">
+                    <div className="flex sm:flex-col justify-evenly items-center">
+                        <div className="flex flex-col font-sans justify-center py-10">
+                            <h4 className="font-sans text-base text-[#616161] font-semibold">Current Plan</h4>
+                            <p className="text-[#252524] font-semibold text-lg flex items-center"><span className="text-[#81C167] text-2xl"><IoCheckmarkCircle /></span>Enterprise Hubs Virtual Office</p>
+                            <h4 className="text-3xl font-bold text-[#252524] my-3">₦107,500/annum</h4>
+                            <p className="text-sm font-light text-[#252524]">Next billing date is 24th September, 2024</p>
+                        </div>
+                        <div className="flex flex-col">
+                            <Link className="flex justify-center items-center border border-[#CBCBCB] w-60 h-[57px] text-[#252524] font-medium bg-[#F4F4F4] p-3 w-40 h-12 rounded-md ">
+                                View Receipt📄
+                            </Link>
+                            <Link className="flex justify-center items-center mt-3 border border-[#CBCBCB] w-60 h-[57px] text-[#E8332C] font-medium bg-[#F4F4F4] p-3 w-40 h-12 rounded-md ">
+                                Cancel Plan
+                            </Link>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default UserProfileEdit;
